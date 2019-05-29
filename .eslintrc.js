05ae56be2451e741e76d94554e6b8d3af2ba2df1
Eliyahu hillel <hillel.eli396@gmail.com>
2019-05-29 18:48:57 +0300

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,20 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  extends:  [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'airbnb', 'prettier'
-  ],
-  parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
-  },
-  rules:  {
-    "max-len": ["error", {"code": 120}],
-    "react/jsx-indent": ["off"],
-    "react/jsx-filename-extension": ["off"],
-    "react/prop-types": ["off"],
-    "import/no-extraneous-dependencies": ["error", {
-      "devDependencies": [
-        ".storybook/**",
-        "stories/**",
-      ]
-    }],
-  },
-  settings:  {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
     },
-    "import/resolver": {
-      "typescript": {},
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    react:  {
-      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            tsx: true,
+        },
+        ecmaVersion: 2018,
     },
-  },
-  plugins: ["import"],
+    plugins: ['react'],
+    rules: {},
 };
