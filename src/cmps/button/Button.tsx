331460c507246523eb7959918ffb75a13d6649a7
Eliyahu hillel <hillel.eli396@gmail.com>
2019-06-21 04:07:03 +0300

    changed by pr button component

--- src/cmps/button/Button.tsx
+++ src/cmps/button/Button.tsx
@@ -1,31 +1,25 @@
 import React, { MouseEvent } from 'react';
 import './Button.css';
 
-// without state use react stateless function
 type props = {
-    isDisabled?: boolean;
-    value: string;
-    onClick(e: MouseEvent<HTMLElement>): void;
+  isDisabled?: boolean;
+  value: string;
+  onClick(e: MouseEvent<HTMLElement>): void;
 };
 
-const Button: React.FC<props> = ({ isDisabled, value, onClick: handleClick }) => {
-    var styleDisable: string = '';
-    if (isDisabled) {
-        styleDisable = 'default';
-    } else {
-        styleDisable = 'disabled';
-    }
-    const classes: string = `button ${value} ${styleDisable}`;
-    /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
-    console.log(value);
-    console.log(styleDisable);
-    return (
-        <div className="buttons">
-            <button onClick={handleClick} className={classes}>
-                {value}
-            </button>
-        </div>
-    );
+const Button: React.FC<props> = ({ isDisabled, value, onClick }) => {
+  var styleDisable: string = '';
+  isDisabled ? (styleDisable = 'default') : (styleDisable = 'disabled');
+
+  const classes: string = `button ${value} ${styleDisable}`;
+  /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+  console.log(value);
+  console.log(styleDisable);
+  return (
+    <button onClick={onClick} className={classes}>
+      {value}
+    </button>
+  );
 };
 
 export default Button;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -12,31 +12,31 @@ import Tag from '../src/cmps/tag/Tag';
 library.add(faStroopwafel);
 
 storiesOf('Button', module)
-    .addDecorator(withKnobs)
-    .add('catalogue', () => (
-        <>
-            <h1>Primary</h1>
-            <Button value="primary" isDisabled />
-            <h1>Primary Disabled</h1>
-            <Button value="primary" isDisabled={false} />
-            <h1>Secondary</h1>
-            <Button value="secondary" isDisabled />
-            <h1>Secondary Disabled</h1>
-            <Button value="secondary" isDisabled={false} />
-            <h1>Tertiary</h1>
-            <Button value="tertiary" isDisabled />
-            <h1>Tertiary Disabled</h1>
-            <Button value="tertiary" isDisabled={false} />
-        </>
-    ))
-    .add('playground', () => <Button value={text('value', 'primary')} isDisabled={boolean('isDisabled', true)} />);
+  .addDecorator(withKnobs)
+  .add('catalogue', () => (
+    <>
+      <h1>Primary</h1>
+      <Button value="primary" isDisabled />
+      <h1>Primary Disabled</h1>
+      <Button value="primary" isDisabled={false} />
+      <h1>Secondary</h1>
+      <Button value="secondary" isDisabled />
+      <h1>Secondary Disabled</h1>
+      <Button value="secondary" isDisabled={false} />
+      <h1>Tertiary</h1>
+      <Button value="tertiary" isDisabled />
+      <h1>Tertiary Disabled</h1>
+      <Button value="tertiary" isDisabled={false} />
+    </>
+  ))
+  .add('playground', () => <Button value={text('value', 'primary')} isDisabled={boolean('isDisabled', true)} />);
 
 storiesOf('Tags')
-    .addDecorator(withKnobs)
-    .add('catalogue', () => (
-        <>
-            <h1>Tags</h1>
-            <Tags />
-        </>
-    ))
-    .add('playground', () => <Tag value={text('value', 'I am tag')} isDeletable={boolean('isDeletable', false)} />);
+  .addDecorator(withKnobs)
+  .add('catalogue', () => (
+    <>
+      <h1>Tags</h1>
+      <Tags />
+    </>
+  ))
+  .add('playground', () => <Tag value={text('value', 'I am tag')} isDeletable={boolean('isDeletable', false)} />);
