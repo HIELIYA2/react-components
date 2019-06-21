296685e7058a303e566d70e14ede3364f7e620e0
Eliyahu hillel <hillel.eli396@gmail.com>
2019-06-21 03:56:54 +0300

    Changed according to the notes of pull requests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
     },
-    extends: 'eslint:recommended',
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            tsx: true,
-        },
-        ecmaVersion: 2018,
-    },
-    plugins: ['react'],
-    rules: {
-        'no-unused-vars': 'off', // TODO remove
-    },
+    ecmaVersion: 2018,
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'off', // TODO remove
+  },
 };
