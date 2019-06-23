9d2ccf73f0ac548d2c08f3b5e255fe0d0f9f711f
Eliyahu hillel <hillel.eli396@gmail.com>
2019-06-23 18:49:56 +0300

    fix to async tags component

--- src/cmps/tags/Tags.tsx
+++ src/cmps/tags/Tags.tsx
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import './Tags.css';
 import Tag from '../tag/Tag';
 
-class Tags extends Component {
+interface ITag {
+  id: string;
+  title: string;
+}
+
+interface State {
+  currentTag: string;
+  tags: ITag[];
+}
+
+interface Props {}
+
+class Tags extends Component<Props, State> {
   state = {
     currentTag: '',
     tags: [],
@@ -10,11 +22,10 @@ class Tags extends Component {
 
   remove = (id: string) => {
     let index = this.state.tags.findIndex((element: any) => element.id === id);
-    this.setState(this.state.tags.splice(index, 1));
-    // this.setState(state => ({ tags: state.tags.splice(index, 1) }));
+    this.setState(state => ({ tags: state.tags.splice(index, 1) }));
   };
 
-  handleSubmit = (e: any) => {
+  handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
     let isDouble = this.checkDouble(this.state.currentTag);
     if (isDouble) {
@@ -43,22 +54,26 @@ class Tags extends Component {
 
   makeId(length: number): string {
     var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; // use REGEX
     for (var i = 0; i < length; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
   }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ currentTag: e.target.value });
+  };
 
   render() {
     return (
       <section>
         <div className="tags-container">
-          <form className="tags-form" onSubmit={e => this.handleSubmit(e)}>
+          <form className="tags-form" onSubmit={this.handleSubmit}>
             <input
               type="text"
               placeholder="Add a tag"
               className="tags-input"
               value={this.state.currentTag}
-              onChange={e => this.setState({ currentTag: e.target.value })}
+              onChange={this.handleChange}
             />
             <button className="tags-add-button" type="submit">
               +
@@ -76,13 +91,5 @@ class Tags extends Component {
     );
   }
 }
-
-interface ITag {
-  id: string;
-  title: string;
-}
-// interface MyState {
-//     tags: Array<{ id: string; isDeletable: boolean; value: string }>;
-// }
 
 export default Tags;
